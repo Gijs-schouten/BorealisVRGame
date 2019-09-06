697a30ffbc109a0da5cff43abf62222e92c6421a
Gijs Schouten <21969@ma-web.nl>
2019-09-06 11:09:40 +0200

    assets

---
+++ Assets/Editor/Extention/GPUInstancerBillboardExtention.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace GPUInstancer.Extention
+{
+    public abstract class GPUInstancerBillboardExtention
+    {
+        public abstract string GetTitle();
+
+        public abstract string GetButtonText();
+
+        public abstract GameObject GenerateBillboard(GameObject gameObject);
+
+        public abstract Mesh GetBillboardMesh(GameObject gameObject);
+
+        public abstract Material GetBillboardMaterial(GameObject gameObject);
+
+        public abstract bool IsInLODGroup(GameObject gameObject);
+
+        public abstract bool IsBillboardAdded(GameObject gameObject);
+    }
+}
