3d9ca18efa23a4d769d52b89feb4d28154e0291f
Gijs Schouten <21969@ma-web.nl>
2019-12-12 14:33:07 +0100

    vr test

---
+++ Assets/cool.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cool : MonoBehaviour {
+	public GameObject lol;
+	
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Y)) {
+			lol.SetActive(true);
+		}
+	}
+}
